ba044d670fa6e74d3a43f40ae8583ba0b2eeb09a
angel <angelbecerrah548@gmail.com>
2020-09-20 14:08:27 -0500

    Feat(server): set up base server

--- server/src/database/connect.js
+++ server/src/database/connect.js
@@ -2,7 +2,10 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 // eslint-disable-next-line no-undef
-module.exports = mongoose.connect(process.env.MONG_URL, (err) => {
+module.exports = mongoose.connect(process.env.MONG_URL, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+}, (err) => {
   if (err) throw err;
   else console.log('Connected to DB');
 });
