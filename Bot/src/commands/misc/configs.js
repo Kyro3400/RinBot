6f2ca0f6b3d8b06419f8fff6d895c6cdbd995f10
angel <angelbecerrah548@gmail.com>
2020-09-25 12:58:17 -0500

    feat(command): configs sub command
    add see sub command shows guild configs

--- Bot/src/commands/misc/configs.js
+++ Bot/src/commands/misc/configs.js
@@ -1,3 +1,4 @@
+const { MessageEmbed } = require('discord.js');
 const command = require('../../structures/BaseCommand.js');
 
 module.exports = class Configs extends command {
@@ -9,14 +10,38 @@ module.exports = class Configs extends command {
     });
   }
 
-  run(client, message, [sub]) {
-    if (!sub) return;
+  async run(client, message, [sub]) {
+    const Nothing = new MessageEmbed();
+    if (!sub) return await message.channel.send(Nothing);
     switch (sub) {
     case 'see': {
+      const {
+        prefix,
+        language,
+        adminRoles,
+        ignoredChannels,
+        autoDeleteModCommands,
+        disabledCategories,
+        autoDeleteLinks,
+        autoDeleteInvites
+      } = message.guild.data;
+      
+      const seeEmbed = new MessageEmbed()
+        .setAuthor('Configs', client.user.avatarURL())
+        .addField('Language', language, true)
+        .addField('Admin Roles', adminRoles.length != 0 ? adminRoles.join(', ') : 'Non', true)
+        .addField('Ignored Channels', ignoredChannels.length != 0 ? ignoredChannels.join(', ') : 'Non', true)
+        .addField('Disabled Categories', disabledCategories.length != 0 ? disabledCategories.join(', ') : 'Non', true)
+        .addField('Delete Mod Commands', autoDeleteModCommands ? 'Yes' : 'No', true)
+        .addField('Delete Links', autoDeleteLinks ? 'Yes' : 'No', true)
+        .addField('Delete Invites', autoDeleteInvites ? 'Yes' : 'No', true)
+        .setFooter(`Guild prefix: ${prefix}`, message.guild.iconURL({ dynamic: true }))
+        .setTimestamp();
+      await message.channel.send(seeEmbed);
       break;
     }
     default: {
-      return;
+      await message.channel.send(Nothing);
     }
     }
   }
